c5799f777b0069713c159c05200c55d9e161abdf
Devon Martens <devonmartens@MacBook-Pro.local>
2022-05-19 13:07:59 -0400

    adds profile connect

--- nft-marketplace/components/navbar/index.tsx
+++ nft-marketplace/components/navbar/index.tsx
@@ -97,30 +97,10 @@ export default function Example() {
                       <Menu.Item>
                         {({ active }) => (
                           <a
-                            href="#"
+                            href="/profile"
                             className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
                           >
                             Your Profile
-                          </a>
-                        )}
-                      </Menu.Item>
-                      <Menu.Item>
-                        {({ active }) => (
-                          <a
-                            href="#"
-                            className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
-                          >
-                            Settings
-                          </a>
-                        )}
-                      </Menu.Item>
-                      <Menu.Item>
-                        {({ active }) => (
-                          <a
-                            href="#"
-                            className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
-                          >
-                            Sign out
                           </a>
                         )}
                       </Menu.Item>
---
+++ nft-marketplace/components/profile.tsx
@@ -0,0 +1,15 @@
+import exp from "constants";
+import { NextPage } from "next";
+import { BaseLayout } from "../components";
+
+
+
+const Profile: NextPage = () => {
+    return(
+        <BaseLayout>
+        some text to prove i work - profile page
+        </BaseLayout>
+    )
+}
+
+export default Profile;
