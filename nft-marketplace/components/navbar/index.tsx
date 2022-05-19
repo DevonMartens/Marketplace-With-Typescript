6f84e76aa01b9fe05224dd08999d44680459f129
Devon Martens <devonmartens@MacBook-Pro.local>
2022-05-19 10:46:53 -0400

    adds properly working nav bar

--- nft-marketplace/components/navbar/index.tsx
+++ nft-marketplace/components/navbar/index.tsx
@@ -4,10 +4,9 @@ import { Disclosure, Menu, Transition } from '@headlessui/react'
 import { BellIcon, MenuIcon, XIcon } from '@heroicons/react/outline'
 
 const navigation = [
-  { name: 'Dashboard', href: '#', current: true },
-  { name: 'Team', href: '#', current: false },
-  { name: 'Projects', href: '#', current: false },
-  { name: 'Calendar', href: '#', current: false },
+  { name: 'Marketplace', href: '/', current: true },
+  { name: 'Create', href: '/create/nft', current: false },
+
 ]
 
 function classNames(...classes: string[]) {
