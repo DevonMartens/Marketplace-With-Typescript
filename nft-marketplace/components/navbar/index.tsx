6d42143f9a5dae64a614c825404c30a6d1ae0068
Devon Martens <devonmartens@MacBook-Pro.local>
2022-05-20 09:00:00 -0400

    installs tailwind forms

--- nft-marketplace/components/navbar/index.tsx
+++ nft-marketplace/components/navbar/index.tsx
@@ -119,8 +119,6 @@ export default function Example() {
                 activeClass='bg-gray-900'
                 >
                 <a
-                
-                  
                   className={'text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium'
                   }
                   aria-current={item.current ? 'page' : undefined}
