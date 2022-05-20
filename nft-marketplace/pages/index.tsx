33439e012876d53118fb73b58129a3543af420e3
Devon Martens <devonmartens@MacBook-Pro.local>
2022-05-20 14:21:28 -0400

    adds ethers

--- nft-marketplace/pages/index.tsx
+++ nft-marketplace/pages/index.tsx
@@ -6,8 +6,8 @@ import { NftMeta } from 'types/nft';
 import { useWeb3 } from 'components/providers/web3';
 
 const Home: NextPage = () => {
-  const { data } = useWeb3();
-  
+  const { test } = useWeb3();
+
   return (
     <BaseLayout>
     <div className="relative bg-gray-50 pt-16 pb-20 px-4 sm:px-6 lg:pt-24 lg:pb-28 lg:px-8">
