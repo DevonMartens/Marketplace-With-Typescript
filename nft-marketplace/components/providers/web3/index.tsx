94ef3ca4c64de92577c949fefc5ccaa337505815
Devon Martens <devonmartens@MacBook-Pro.local>
2022-05-20 13:03:47 -0400

    fixes issue

--- nft-marketplace/components/providers/web3/index.tsx
+++ nft-marketplace/components/providers/web3/index.tsx
@@ -1,19 +1,27 @@
-import {  createContext, FunctionComponent, useState } from "react";
+import * as React from "react";
+import {  ReactNode, createContext, FunctionComponent, useContext, useState } from "react";
 
-
+interface MyProps {
+    children?: ReactNode;
+}
 
 
 const Web3Context = createContext<any>(null);
 
 
+const Web3Provider: React.FC<MyProps>  = ({children}) => {
+    const [web3Api, setWeb3Api] = useState({test: "connect"})
 
-const Web3Provider: FunctionComponent = {(children) => {
-
-const [web3Api, setWeb3Api] = useState({test: "connect"})
 
 return(
 <Web3Context.Provider value={web3Api}>
-{children}
+    {children}
 </Web3Context.Provider>
     )
 }
+
+export function useWeb3() {
+    return useContext(Web3Context);
+}
+
+export default Web3Provider;
