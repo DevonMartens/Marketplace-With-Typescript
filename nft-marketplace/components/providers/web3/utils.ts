9b92b3a30f918570c4c86f13763e76a61fe12741
Devon Martens <devonmartens@MacBook-Pro.local>
2022-05-21 10:17:09 -0400

    sets a defual state of loading & null contract/provider/ethereum

--- nft-marketplace/components/providers/web3/utils.ts
+++ nft-marketplace/components/providers/web3/utils.ts
@@ -7,3 +7,16 @@ export type Web3Params = {
     provider: providers.Web3Provider | null;
     contract: Contract | null;
 }
+
+export type Web3State = {
+    isLoading: boolean; // this is only true when web3 is loading
+} & Web3Params
+
+export const createDefaultState = () => {
+    return {
+        ethereum: null,
+        provider: null,
+        contract: null,
+        isLoading: true,
+    }
+}
