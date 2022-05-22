4d90bab6019d0e7a5874057632c8d5ae8301d68e
Devon Martens <devonmartens@MacBook-Pro.local>
2022-05-22 19:34:46 -0400

    cleans config, messing with contracts off github

--- nft-marketplace/contracts/Migrations.sol
+++ nft-marketplace/contracts/Migrations.sol
@@ -1,16 +1,9 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.4;
 
-import "@openzeppelin/contracts-upgradeable/token/ERC721/ERC721Upgradeable.sol";
-import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+pragma solidity >=0.8.9 <0.9.0;
 
-contract MyToken is Initializable, ERC721Upgradeable {
-    /// @custom:oz-upgrades-unsafe-allow constructor
-    constructor() {
-        _disableInitializers();
-    }
+// import 'erc721a/contracts/ERC721A.sol';
+import '@openzeppelin/contracts/access/Ownable.sol';
+// ERC721A,
+contract MEB is  Ownable {}
 
-    function initialize() initializer public {
-        __ERC721_init("MyToken", "MTK");
-    }
-}
