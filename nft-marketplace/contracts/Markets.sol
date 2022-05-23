92e6f7b22f91fd717cfb05960155bac1782e9046
Devon <dmmartens91@gmail.com>
2022-05-23 14:53:46 -0400

    adds files

--- nft-marketplace/contracts/Markets.sol
+++ nft-marketplace/contracts/Markets.sol
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.4.22 <0.9.0;
 
-contract SWIMMarketPlace {
+import "../node_modules/@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 
+contract SWIMMarketPlace is ERC721URIStorage {
+  constructor() ERC721("SWIMMarketPlace", "SWIM") {}
 }
