db6065a979ec11e68ae20cdd696d6f458b85fc87
Devon <dmmartens91@gmail.com>
2022-05-24 13:36:30 -0400

    begins set up

--- components/hooks/web3/useAccount.ts
+++ components/hooks/web3/useAccount.ts
@@ -4,9 +4,12 @@ import useSWR from "swr";
 
 //deps -> provider, ethereum, contract (web3State)
 
-export const hookFactory = ({...deps}) => () => {
+export const hookFactory = (deps: any) => (params: any) => {
   const swrRes =  useSWR("web3/useAccount", () => {
+     console.log(deps);
         return "Test User"
     })
     return swrRes;
 }
+
+export const useAccount = hookFactory({ethereum: null, provider: null});
