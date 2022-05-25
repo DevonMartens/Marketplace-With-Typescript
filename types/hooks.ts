30e6e7b7f0a8024ee8764f7e66baaa2101cfee28
Devon <dmmartens91@gmail.com>
2022-05-25 12:41:10 -0400

    working on hook

--- types/hooks.ts
+++ types/hooks.ts
@@ -1,6 +1,7 @@
 import { MetaMaskInpageProvider } from "@metamask/providers";
 import { Contract, providers } from "ethers";
 import { type } from "os";
+import { SWRResponse } from "swr";
 
 export type Web3Dependencies = {
     provider: providers.Web3Provider;
@@ -8,8 +9,10 @@ export type Web3Dependencies = {
     ethereum: MetaMaskInpageProvider;
 }
 
-export type Web3Dependencies = {
-    
+export type CryptoSWRResponse = SWRResponse;
+
+export type CryptoHandlerHook = () => CryptoSWRResponse 
+
 export type CryptoHookFactory = {
     (d: Partial<Web3Dependencies>): void
 }
