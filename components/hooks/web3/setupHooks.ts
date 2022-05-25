e779b614618bde91369d522034eb4fbcf2de2a3e
Devon <dmmartens91@gmail.com>
2022-05-25 16:21:48 -0400

    adds hook

---
+++ components/hooks/web3/setupHooks.ts
@@ -0,0 +1,17 @@
+
+import { Web3Dependencies } from "types/hooks";
+import { hookFactory as createAccountHook, useAccountHook } from "./useAccount";
+
+export type Web3Hooks = {
+  useAccount: useAccountHook;
+}
+
+export type SetupHooks = {
+  (d: Web3Dependencies): Web3Hooks
+}
+
+export const setupHooks: SetupHooks = (deps) => {
+  return {
+    useAccount: createAccountHook(deps)
+  }
+}
--- components/hooks/web3/useAccount.ts
+++ components/hooks/web3/useAccount.ts
@@ -6,16 +6,18 @@ import { ContractFactory } from "ethers";
 type AccountHookFactory = CryptoHookFactory<string, string>
 //deps -> provider, ethereum, contract (web3State)
 
-type useAccountHook = ReturnType<AccountHookFactory>
+//type useAccountHook = ryptoHookFactory<string, string>
 //type ReturnType 
 
 
-export const hookFactory:AccountHookFactory = (deps) => (params) => {
-
-  const swrRes =  useSWR("web3/useAccount", () => {
-     console.log(deps);
-        return "Test User"
-    })
+export type useAccountHook = ReturnType<AccountHookFactory>
+export const hookFactory: AccountHookFactory = (deps) => (params) => {
+  const swrRes = useSWR("web3/useAccount", () => {
+    console.log(deps);
+    console.log(params);
+    // making request to get data
+    return "Test User"
+  })
     return swrRes;
 }
 
